1e1067ec3659791ab58a984a11301e6fa587f78f
tim <llStrainll@gmail.com>
2014-10-17 16:43:47 +0400

    fix nodejs on ubuntu

--- docker_image/ubuntu_iced.sh
+++ docker_image/ubuntu_iced.sh
@@ -6,5 +6,6 @@ apt-get update
 apt-get -y install esl-erlang
 apt-get -y install make gcc g++
 apt-get -y install npm; npm install -g iced-coffee-script
+ln -s /usr/bin/nodejs /usr/bin/node
 
 cd /opt; git clone https://github.com/elixir-lang/elixir.git;cd elixir; git checkout v1.0.0; make clean install; cd /opt; rm -rf ./elixir
